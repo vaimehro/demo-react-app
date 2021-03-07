83e99af90ceec556e3df77bb8aeb7b4f9506eb91
vaimehro <vaibhavmehrotrapccs@gmail.com>
2021-03-07 21:47:20 +0530

    Updates

---
+++ precache-manifest.a815071061857d515ace5d52fab41b89.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd613cf6322426f7f788b6131b97ec58",
+    "url": "/demo-react-app/index.html"
+  },
+  {
+    "revision": "1fd8a5e80cb0fcdb868c",
+    "url": "/demo-react-app/static/css/2.0f8e233a.chunk.css"
+  },
+  {
+    "revision": "29b074e60562412b59b0",
+    "url": "/demo-react-app/static/css/main.aa519aab.chunk.css"
+  },
+  {
+    "revision": "1fd8a5e80cb0fcdb868c",
+    "url": "/demo-react-app/static/js/2.cdf47e15.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/demo-react-app/static/js/2.cdf47e15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b074e60562412b59b0",
+    "url": "/demo-react-app/static/js/main.ed06fd64.chunk.js"
+  },
+  {
+    "revision": "22b6799279cf9fa25a15",
+    "url": "/demo-react-app/static/js/runtime-main.e9830cc7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
