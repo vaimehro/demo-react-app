959ae21c9e0bb052c05e36a6a1776476933b687d
vaimehro <vaibhavmehrotrapccs@gmail.com>
2021-04-21 16:33:47 +0530

    css changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.css';
-// Put any other imports below so that CSS from your
-// components takes precedence over default styles.
 
-import {Route, BrowserRouter as Router, Switch, Link, PropsRoute} from "react-router-dom"
-import ListGroup from 'react-bootstrap/ListGroup'
-import Tabs from 'react-bootstrap/Tabs'
-import Nav from 'react-bootstrap/Nav'
-import NavDropdown from 'react-bootstrap/NavDropdown'
-import Navbar from 'react-bootstrap/Navbar'
-import NavLink from 'react-bootstrap/NavLink'
-import FormControl from 'react-bootstrap/FormControl'
+
+import {Route, BrowserRouter as Router, Switch, Link, PropsRoute } from "react-router-dom";
+import {Nav, NavDropdown, Navbar, NavLink, NavItem, Container,FormControl,Button} from "react-bootstrap";
 
 import Greet from "./Components/Greet";
 import Welcome from "./Components/Welcome";
@@ -31,92 +24,132 @@ import {UserProvider} from "./Components/userContext";
 import ComponentC from "./Components/ComponentC";
 import FragmentDemo from "./Components/FragmentDemo";
 import PortalParent from "./Components/PortalParent";
-
+import OrgSideBar from "./Components/OrgSideBar";
 import './App.css';
 import UserList from "./Components/UserList";
 import HigherOrderComponent from "./Components/HigherOrderComponent";
+
 function App() {
     return (
         <div className="App">
-        <Router >
-            <Navbar  expand="lg"  bg="info" variant="dark" >
-                <Navbar.Brand href="#home">My Site</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Router>
 
-                <Navbar.Collapse id="basic-navbar-nav" >
-                    <Nav className="mr-auto">
-                        <NavLink as={Link} to="/UserList">User API</NavLink>
-                        <Nav.Link as={Link}  to="/GreetFunctionComponent">Home </Nav.Link>
-                        <Nav.Link as={Link}  to="/WelcomeClassComponent">Link</Nav.Link>
-                        <NavLink as={Link} to="/GreetFunctionComponent">See Function Component Greetings</NavLink>
-                        <NavLink as={Link} to="/WelcomeClassComponent">See Class Component Welcome</NavLink>
-                        <NavLink as={Link} to="/StateExampleComponent">Change State Example With Subscribe Button</NavLink>
-                        <NavLink as={Link} to="/SetStateCounter">Counter Using Set State and Previous State !!</NavLink>
-                        <NavDropdown title="Important Links"  id="collasible-nav-dropdown" >
-                            <NavLink as={Link} to="/EventBindInClassComponent">Event Bind In Class Component</NavLink>
-                            <NavDropdown.Divider />
-                            <NavLink as={Link} to="/AccessFunctionParentComponentFromChildComponent">
-                                Access Function f Parent Component From Child Component
-                            </NavLink>
-                            <NavDropdown.Divider />
-                            <NavLink as={Link} to="/ListRenderingComponent">
-                                List Rendering By Creating Single Component as Person and Re using It in Person List
-                            </NavLink>
-                            <NavDropdown.Divider />
-                            <NavLink as={Link} to="/Form"> Form Elements </NavLink>
-                            <NavLink as={Link} to="/LifeCycleA">Check Order of Mounting Phase Method</NavLink>
-                            <NavLink as={Link} to="/FragmentDemo">Fragment Demo, Use &lt;&gt; &lt;/&gt; </NavLink>
-                            <NavLink as={Link} to="/PortalParent">Portal Demo with Modal</NavLink>
-                            <NavLink as={Link} to="/HOCExample">HOC Example </NavLink>
-                            <NavLink as={Link} to="/ContextApi">Context Api </NavLink>
+                <Container>
+                    <div className="sidebar-container">
+                        <div className="sidebar-logo">
+                            Project Name
+                        </div>
+                        <ul className="sidebar-navigation">
+                            <li className="header">Navigation</li>
+                            <li>
+                                <a href="/UserList">
+                                    <i className="fa fa-home" aria-hidden="true"></i> Homepage
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i className="fa fa-tachometer" aria-hidden="true"></i> Dashboard
+                                </a>
+                            </li>
+                            <li className="header">Another Menu</li>
+                            <li>
+                                <a href="#">
+                                    <i className="fa fa-users" aria-hidden="true"></i> Friends
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i className="fa fa-cog" aria-hidden="true"></i> Settings
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <i className="fa fa-info-circle" aria-hidden="true"></i> Information
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    {/*<Navbar collapseOnSelect bg="dark" variant="dark">*/}
+                    {/*    <Navbar.Brand href="#home">My Site</Navbar.Brand>*/}
+                    {/*    <Navbar.Toggle aria-controls="basic-navbar-nav"/>*/}
 
-                        </NavDropdown>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+                    {/*    <Navbar.Collapse id="basic-navbar-nav" >*/}
+                    {/*        <Nav fill variant="tabs" className="mr-auto">*/}
+                    {/*            <NavLink as={Link} to="/UserList">User API</NavLink>*/}
+                    {/*            <Nav.Link as={Link}  to="/GreetFunctionComponent">Home </Nav.Link>*/}
+                    {/*            <Nav.Link as={Link}  to="/WelcomeClassComponent">Link</Nav.Link>*/}
+                    {/*            <NavLink as={Link} to="/GreetFunctionComponent">See Function Component Greetings</NavLink>*/}
+                    {/*            <NavLink as={Link} to="/WelcomeClassComponent">See Class Component Welcome</NavLink>*/}
+                    {/*            <NavLink as={Link} to="/StateExampleComponent">Change State Example With Subscribe Button</NavLink>*/}
+                    {/*            <NavLink as={Link} to="/SetStateCounter">Counter Using Set State and Previous State !!</NavLink>*/}
+                    {/*            <NavDropdown title="Important Links"  id="collasible-nav-dropdown" >*/}
+                    {/*                <NavLink as={Link} to="/EventBindInClassComponent">Event Bind In Class Component</NavLink>*/}
+                    {/*                <NavDropdown.Divider />*/}
+                    {/*                <NavLink as={Link} to="/AccessFunctionParentComponentFromChildComponent">*/}
+                    {/*                    Access Function f Parent Component From Child Component*/}
+                    {/*                </NavLink>*/}
+                    {/*                <NavDropdown.Divider />*/}
+                    {/*                <NavLink as={Link} to="/ListRenderingComponent">*/}
+                    {/*                    List Rendering By Creating Single Component as Person and Re using It in Person List*/}
+                    {/*                </NavLink>*/}
+                    {/*                <NavDropdown.Divider />*/}
+                    {/*                <NavLink as={Link} to="/Form"> Form Elements </NavLink>*/}
+                    {/*                <NavLink as={Link} to="/LifeCycleA">Check Order of Mounting Phase Method</NavLink>*/}
+                    {/*                <NavLink as={Link} to="/FragmentDemo">Fragment Demo, Use &lt;&gt; &lt;/&gt; </NavLink>*/}
+                    {/*                <NavLink as={Link} to="/PortalParent">Portal Demo with Modal</NavLink>*/}
+                    {/*                <NavLink as={Link} to="/HOCExample">HOC Example </NavLink>*/}
+                    {/*                <NavLink as={Link} to="/ContextApi">Context Api </NavLink>*/}
 
-            <div className="myAppRoutes">
+                    {/*            </NavDropdown>*/}
+                    {/*        </Nav>*/}
+                    {/*    </Navbar.Collapse>*/}
+                    {/*</Navbar>*/}
 
-                <Switch>
-                    <Route path="/GreetFunctionComponent" children={<Greet name={"Vaibhav"}/>}></Route>
-                    <Route path="/WelcomeClassComponent" component={Welcome}></Route>
-                    <Route path="/StateExampleComponent" component={Message}></Route>
-                    <Route path="/SetStateCounter" component={Counter}></Route>
-                    <Route path="/EventBindInClassComponent" component={EventBind}></Route>
-                    <Route path="/AccessFunctionParentComponentFromChildComponent" component={ParentComponent}></Route>
-                    <Route path="/ListRenderingComponent" component={NameList}></Route>
-                    <Route path="/Form" component={Form}></Route>
-                    <Route path="/LifeCycleA" component={LifeCycleA}></Route>
-                    <Route path="/FragmentDemo" component={FragmentDemo}></Route>
-                    <Route path="/PortalParent" component={PortalParent}></Route>
-                    <Route path="/HOCExample" component={ClickCounter}></Route>
-                    <Route path="/ContextApi" component={ComponentC}></Route>
-                    <Route path="/UserList" component={UserList}></Route>
+                    <Navbar bg="primary" variant="dark">
+                        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Form inline>
+                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                            <Button variant="outline-light">Search</Button>
+                        </Form>
+                    </Navbar>
+
+                    <div className="content-container shadow-lg p-3 mb-5 bg-white rounded">
+
+                        <div className="container-fluid">
+
+                            <Switch>
+                                <Route path="/GreetFunctionComponent" children={<Greet name={"Vaibhav"}/>}></Route>
+                                <Route path="/WelcomeClassComponent" component={Welcome}></Route>
+                                <Route path="/StateExampleComponent" component={Message}></Route>
+                                <Route path="/SetStateCounter" component={Counter}></Route>
+                                <Route path="/EventBindInClassComponent" component={EventBind}></Route>
+                                <Route path="/AccessFunctionParentComponentFromChildComponent"
+                                       component={ParentComponent}></Route>
+                                <Route path="/ListRenderingComponent" component={NameList}></Route>
+                                <Route path="/Form" component={Form}></Route>
+                                <Route path="/LifeCycleA" component={LifeCycleA}></Route>
+                                <Route path="/FragmentDemo" component={FragmentDemo}></Route>
+                                <Route path="/PortalParent" component={PortalParent}></Route>
+                                <Route path="/HOCExample" component={ClickCounter}></Route>
+                                <Route path="/ContextApi" component={ComponentC}></Route>
+                                <Route path="/UserList" component={UserList}></Route>
+
+                            </Switch>
 
 
+                        </div>
+                    </div>
+                </Container>
 
-                </Switch>
-                {/*<Greet name="vaibhav"></Greet>*/}
-                {/*     <Greet name="tushar"></Greet>*/}
-                {/*     <Welcome></Welcome>*/}
-                {/*     <Message></Message>*/}
-                {/*     <Counter></Counter>*/}
-                {/*<EventBind></EventBind>*/}
-                {/*<FunctionClick></FunctionClick>*/}
-                {/*     <ClassClick></ClassClick>*/}
-                {/*     <ParentComponent></ParentComponent>*/}
-                {/*     <NameList></NameList>*/}
-                {/*  <Form></Form>*/}
-                {/*<LifeCycleA></LifeCycleA>*/}
-                {/*<PureComp></PureComp>*/}
-                {/*<RefsDemo></RefsDemo>*/}
-                {/*<ClickCounter></ClickCounter>*/}
-                {/*<UserProvider value="Vaibhav"> <ComponentC></ComponentC>  </UserProvider>*/}
+            </Router>
+            <HigherOrderComponent></HigherOrderComponent>
 
 
-            </div>
-        </Router>
-        <HigherOrderComponent></HigherOrderComponent>
         </div>
     );
 }
